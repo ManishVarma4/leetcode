211313c6ed8d68d375adf795982801eb5512a0c8
Manishkumar Varma <168369720+ManishVarma4@users.noreply.github.com>
2026-09-16 19:18:46 +0530

    Solve #141 Linked List Cycle [cpp]

---
+++ leetcode/0141-linked-list-cycle/solution.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *a=head,*b=head;
+        while(a!= NULL && a->next != NULL){
+            a=a->next->next;
+            b=b->next;
+            if(a==b) return 1;
+        }
+        return 0;
+    }
+};
